4503b62a867f70a38252c3923a3d421e9fcebd6d
Tord Johan Espe <tordjes@stud.ntnu.no>
2022-02-15 16:29:50 +0100

    "Finish" UI for registering user

--- frontend/src/containers/RegisterPage/index.js
+++ frontend/src/containers/RegisterPage/index.js
@@ -9,33 +9,62 @@ import {
   Input,
   VStack,
   Divider,
+  FormLabel,
 } from "@chakra-ui/react";
 import { Formik } from "formik";
 import { InputControl, SubmitButton } from "formik-chakra-ui";
 import "./index.css";
 
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  email: Yup.string().required().email("Email is not valid").label("Email"),
+  firstName: Yup.string().required().label("First name"),
+  lastName: Yup.string().required().label("Last name"),
+  password: Yup.string().required().label("Password"),
+  confirmPassword: Yup.string().when("password", {
+    is: (val) => (val && val.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Both passwords need to be the same"
+    ),
+  }),
+});
+
 const RegisterUser = () => {
   return (
     <Center width="100%" height="100vh">
       <Container>
-        <Container>
-          <Heading>Register</Heading>
-        </Container>
-        <Container>
-          <Heading size={5}>
-            Please enter email and password to create a user.
-          </Heading>
-        </Container>
         <Container paddingTop="1em">
-          <VStack spacing={5} align="stretch" divider={<Divider spacing={4} />}>
-            <Formik
-              onSubmit={(values, actions) => {
-                console.log(values);
-              }}
-              initialValues={{}}
-            >
-              {(props) => (
-                <>
+          <Formik
+            onSubmit={(values, actions) => {
+              console.log(values);
+            }}
+            initialValues={{}}
+            validationSchema={validationSchema}
+          >
+            {(props) => (
+              <Box
+                borderWidth="1px"
+                rounded="lg"
+                shadow="1px 1px 3px rgba(0,0,0,0.3)"
+                maxWidth={800}
+                p={6}
+                m="10px auto"
+                as="form"
+                onSubmit={props.handleSubmit}
+              >
+                <VStack spacing={5} align="stretch">
+                  <Box>
+                    <Heading>Register</Heading>
+                  </Box>
+                  <Box>
+                    <Heading size={5}>
+                      Please enter email and password to create a user.
+                    </Heading>
+                  </Box>
+                  {/* <pre>{JSON.stringify(props, null, 4)}</pre> */}
+                  {console.log(props)}
                   <Box>
                     <InputControl
                       type="email"
@@ -66,22 +95,27 @@ const RegisterUser = () => {
                     </Box>
                   </HStack>
                   <Box>
-                    <FormControl name="password" label="Password">
-                      <Input
-                        type="password"
-                        autoComplete="new-password"
-                        required
-                        placeholder="• • • • • • • •"
-                      />
-                      <FormHelperText>Create a new password.</FormHelperText>
-                    </FormControl>
+                    <InputControl
+                      required
+                      name="password"
+                      label="Password"
+                      inputProps={{
+                        type: "password",
+                        autoComplete: "new-password",
+                        placeholder: "• • • • • • • •",
+                      }}
+                      helperText="Create a new password."
+                    />
                   </Box>
                   <Box>
                     <InputControl
-                      type="password"
-                      name="confirmPassword"
-                      placeholder="• • • • • • • •"
                       required
+                      name="confirmPassword"
+                      inputProps={{
+                        type: "password",
+                        autoComplete: "new-password",
+                        placeholder: "• • • • • • • •",
+                      }}
                       helperText="Repeat your password."
                     />
                   </Box>
@@ -93,10 +127,10 @@ const RegisterUser = () => {
                       Register user
                     </SubmitButton>
                   </Box>
-                </>
-              )}
-            </Formik>
-          </VStack>
+                </VStack>
+              </Box>
+            )}
+          </Formik>
         </Container>
       </Container>
     </Center>
