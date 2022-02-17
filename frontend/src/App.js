f4659c1470138854502c73cfc236a773492fdc23
Torje Markussen <torjemar@stud.ntnu.no>
2022-02-17 12:11:10 +0100

    Fixed reference to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import RegisterPage from "./containers/RegisterPage";
+import LoginPage from "./containers/LoginPage";
 import { ChakraProvider } from "@chakra-ui/react";
-import axios from "axios"
+import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.headers.post["Content-Type"] = "application/json";
@@ -17,7 +18,18 @@ function App() {
             path="/"
             element={
               <div>
-                <Link to="/register">Registrer bruker</Link>
+                <Link to="/register">Register new user</Link>
+              </div>
+            }
+          />
+        </Routes>
+        <Routes>
+          <Route path="/users/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Link to="/users/login">Login</Link>
               </div>
             }
           />
