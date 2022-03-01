81e02d9a3654ec81cdaf063d40d005a288e59e16
Idunn Stabell <idunnsta@stud.ntnu.no>
2022-03-01 12:56:03 +0100

    Resolve "#38 Oppdatere feed til å følge design"

--- frontend/src/components/ProfileImage/index.js
+++ frontend/src/components/ProfileImage/index.js
@@ -1,5 +1,17 @@
-function ProfileImage() {
-  return <>Coming soon!</>;
+import { Center, Circle } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+function ProfileImage({ userId }) {
+  console.log(userId);
+  return (
+    <Center w="100%" h="50%" color="white">
+      <Circle size="40px" bg="gray" color="white" alignContent="70%" />
+    </Center>
+  );
 }
+
+ProfileImage.propTypes = {
+  userId: PropTypes.number.isRequired,
+};
 
 export default ProfileImage;
