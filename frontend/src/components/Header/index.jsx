653f312347fa30ca3fed60ab94cb1155f113c3ff
Sankeethan Sivachandran <sankee1204@gmail.com>
2022-03-03 12:40:55 +0100

    Removed unused import

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -14,8 +14,6 @@ import {
   Stack,
   useColorModeValue,
   useDisclosure,
-  Link,
-  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import { NavLink as ReactNavLink, useNavigate } from "react-router-dom";
