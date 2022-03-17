5cbbe2fdc628200bcc1b9bd7a4b8da7c5827bb68
Sankeethan Sivachandran <sankeets@stud.ntnu.no>
2022-03-17 13:23:07 +0100

    Resolve "Som bruker, ønsker jeg å ha tilgang til å redigere eller slette mine innlegg, og at ingen andre har det. PP: Sangi + Tord"

--- frontend/src/components/PostModal/index.jsx
+++ frontend/src/components/PostModal/index.jsx
@@ -1,7 +1,9 @@
-import { EditIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
+  Grid,
+  GridItem,
   Heading,
   HStack,
   IconButton,
@@ -19,63 +21,120 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { useCurrentUser } from "../../contexts/UserInfoContext";
+import postShape from "../../shapes/post";
+import { useFeedContext } from "../Feed/FeedContext";
+import PostForm from "../PostForm";
 import ProfileImage from "../ProfileImage";
+import DeleteConfirmation from "./DeleteConfirmation";
 
+/**
+ * @param post we need to pass a post to the function so that we can show the information we need to display
+ * @param onClose tells the function if the modal window needs to be closed
+ * @returns a modal window which displays information about a single post
+ */
 function PostModalWindow({ post, onClose }) {
   const currentUser = useCurrentUser();
+  const feedContext = useFeedContext();
+  const [editMode, setEditMode] = useState(false);
+  const [deleteMode, setDeleteMode] = useState(false);
   return (
     <Modal isOpen onClose={onClose} size="lg">
       <ModalOverlay />
       <ModalContent bg="#efefef">
-        <ModalHeader>{post.event}</ModalHeader>
+        <ModalHeader>
+          {editMode ? (
+            <Grid templateColumns="repeat(7, 1fr)" gap={4}>
+              <GridItem colSpan={1}>
+                <IconButton
+                  colorScheme="teal"
+                  aria-label="Search database"
+                  icon={<ArrowBackIcon />}
+                  onClick={() => setEditMode(false)}
+                />
+              </GridItem>
+              <GridItem colStart={3} colEnd={7}>
+                {post.event}
+              </GridItem>
+            </Grid>
+          ) : (
+            <Box>{post.event}</Box>
+          )}
+        </ModalHeader>
         <ModalCloseButton />
+        {deleteMode && (
+          <DeleteConfirmation
+            postId={post.id}
+            onClose={() => setDeleteMode(false)}
+          />
+        )}
         <ModalBody padding="0 2em 2em">
-          <VStack spacing={10}>
-            <Box width="100%">
-              <HStack spacing={10} justify="space-between">
-                <Box>
-                  <ProfileImage size="xl" userId={post.user} />
-                </Box>
-                <Box bg="white" borderRadius="5px" padding="1em">
-                  <HStack justify="space-between">
-                    <Heading size={4}>{post.post_type}</Heading>
-                    {post.user === currentUser.id && (
-                      <Box>
-                        <IconButton
-                          aria-label="Edit post"
-                          icon={<EditIcon />}
-                        />
-                      </Box>
-                    )}
-                  </HStack>
-                  <Table>
-                    <Tbody>
-                      <Tr>
-                        <Td>Event:</Td>
-                        <Td>{post.event}</Td>
-                      </Tr>
-                      <Tr>
-                        <Td>Location:</Td>
-                        <Td>{post.location}</Td>
-                      </Tr>
-                      <Tr>
-                        <Td>Price</Td>
-                        <Td>{post.price}</Td>
-                      </Tr>
-                    </Tbody>
-                  </Table>
-                </Box>
-              </HStack>
-            </Box>
-            <Box width="100%" bg="white" borderRadius="5px" padding="1em">
-              <Heading size={5}>Description: </Heading>
-              {post.description || "No description"}
-            </Box>
-          </VStack>
+          {editMode ? (
+            <PostForm
+              post={post}
+              onSubmit={(newValues) => {
+                feedContext.onPostUpdate(newValues);
+                setEditMode(false);
+              }}
+            />
+          ) : (
+            <VStack spacing={10}>
+              <Box width="100%">
+                <HStack spacing={10} justify="space-between">
+                  <Box>
+                    <ProfileImage size="xl" userId={post.user} />
+                  </Box>
+                  <Box bg="white" borderRadius="5px" padding="1em">
+                    <HStack justify="space-between">
+                      <Heading size={4}>{post.post_type}</Heading>
+                      {post.user === currentUser?.id && (
+                        <HStack spacing="2">
+                          <Box>
+                            <IconButton
+                              aria-label="Edit post"
+                              icon={<EditIcon />}
+                              onClick={() => setEditMode(true)}
+                            />
+                          </Box>
+                          <Box>
+                            <IconButton
+                              aria-label="Delete post"
+                              icon={<DeleteIcon />}
+                              onClick={() => setDeleteMode(true)}
+                            />
+                          </Box>
+                        </HStack>
+                      )}
+                    </HStack>
+                    <Table>
+                      <Tbody>
+                        <Tr>
+                          <Td>Event:</Td>
+                          <Td>{post.event}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Location:</Td>
+                          <Td>{post.location}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Price</Td>
+                          <Td>{post.price}</Td>
+                        </Tr>
+                      </Tbody>
+                    </Table>
+                  </Box>
+                </HStack>
+              </Box>
+              <Box width="100%" bg="white" borderRadius="5px" padding="1em">
+                <Heading size={5}>Description: </Heading>
+                {post.description || "No description"}
+              </Box>
+            </VStack>
+          )}
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
+          <Button colorScheme="teal" mr={3} onClick={onClose}>
             Close
           </Button>
         </ModalFooter>
@@ -85,17 +144,7 @@ function PostModalWindow({ post, onClose }) {
 }
 
 PostModalWindow.propTypes = {
-  post: PropTypes.shape({
-    id: PropTypes.number,
-    post_type: PropTypes.oneOf(["BUYING", "SELLING"]),
-    event: PropTypes.string,
-    location: PropTypes.string,
-    description: PropTypes.string,
-    available: PropTypes.bool,
-    price: PropTypes.number,
-    user: PropTypes.number,
-  }).isRequired,
-
+  post: postShape.isRequired,
   onClose: PropTypes.func.isRequired,
 };
 
--- frontend/src/containers/ApplicationPage/AddPostPage/index.js
+++ frontend/src/containers/ApplicationPage/AddPostPage/index.js
@@ -3,37 +3,14 @@ import {
   Button,
   Center,
   Container,
-  Heading,
   HStack,
   StackDivider,
   VStack,
 } from "@chakra-ui/react";
-import axios from "axios";
-import { Formik } from "formik";
-import {
-  InputControl,
-  SelectControl,
-  SubmitButton,
-  TextareaControl,
-} from "formik-chakra-ui";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import * as Yup from "yup";
+import PostForm from "../../../components/PostForm";
 import { useCurrentUser } from "../../../contexts/UserInfoContext";
-import { BUYING, SELLING } from "./constants";
-
-const validationSchema = Yup.object({
-  post_type: Yup.string()
-    .required()
-    .oneOf([SELLING, BUYING])
-    .label("Type of post"),
-  event: Yup.string().required().label("Event name"),
-  price: Yup.number().optional(),
-  description: Yup.string().label("Description"),
-  location: Yup.string().required().label("Location"),
-});
-
-const initialValues = { post_type: SELLING };
 
 function AddPostPage() {
   const navigate = useNavigate();
@@ -73,97 +50,7 @@ function AddPostPage() {
       overflowY="auto"
     >
       <Container height="100%">
-        <Formik
-          onSubmit={(values, { setErrors }) =>
-            // Make a POST request to /users/login/ with email and password to log in
-            axios
-              .post("/posts/", { ...values, user: user.id })
-              .then(() => navigate("/"))
-              // If something goes wrong, set form errors
-              .catch((error) => setErrors(error.response.data))
-          }
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-        >
-          {(formProps) => {
-            const sellOrBuy =
-              formProps.values.post_type === BUYING ? "buy" : "sell";
-            const potentialBuyersOrSellers =
-              formProps.values.post_type === BUYING
-                ? "potential sellers"
-                : "potential buyers";
-
-            return (
-              <Box
-                borderWidth="1px"
-                rounded="lg"
-                shadow="1px 1px 3px rgba(0,0,0,0.3)"
-                maxWidth={800}
-                p={6}
-                m="10px auto"
-                as="form"
-                onSubmit={formProps.handleSubmit}
-                background="#fff"
-              >
-                <VStack spacing={5} align="stretch">
-                  <Box>
-                    <Heading>Create a post</Heading>
-                  </Box>
-                  <Box>
-                    <InputControl
-                      required
-                      name="event"
-                      label="Event"
-                      helperText="The name of the event you are selling or buying a ticket for."
-                    />
-                  </Box>
-                  <Box>
-                    <InputControl
-                      required
-                      name="location"
-                      label="Location"
-                      helperText="Where is the event?"
-                    />
-                  </Box>
-                  <Box>
-                    <SelectControl
-                      name="post_type"
-                      required
-                      label="Buying or selling?"
-                      helperText={`If you want to sell a ticket, select 'Selling', otherwise, select 'Buying'.`}
-                    >
-                      <option value={SELLING}>Selling</option>
-                      <option value={BUYING}>Buying</option>
-                    </SelectControl>
-                  </Box>
-                  <Box>
-                    <InputControl
-                      name="price"
-                      label="Price"
-                      helperText={`The price you are willing to ${sellOrBuy} for.`}
-                      inputProps={{ type: "number" }}
-                    />
-                  </Box>
-                  <Box>
-                    <TextareaControl
-                      name="description"
-                      label="Description"
-                      helperText={`Any other info you want ${potentialBuyersOrSellers} to see.`}
-                    />
-                  </Box>
-                  <Box textAlign="right">
-                    <SubmitButton
-                      isLoading={formProps.isSubmitting}
-                      isDisabled={!formProps.isValid || !formProps.dirty}
-                    >
-                      Create post
-                    </SubmitButton>
-                  </Box>
-                </VStack>
-              </Box>
-            );
-          }}
-        </Formik>
+        <PostForm onSubmit={() => navigate("/")} />
       </Container>
     </HStack>
   );
