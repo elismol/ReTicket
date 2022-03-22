006eb60ff77e1d37a1784cbdbb93f5ae624a340e
Torje Markussen <torjemar@stud.ntnu.no>
2022-03-22 18:07:53 +0100

    Mark as sold by selecting user

--- frontend/src/components/Feed/Post/style.js
+++ frontend/src/components/Feed/Post/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Box } from "@chakra-ui/react";
 
 export const StyledBox = styled(Box)`
-  background-color: ${(props) => (props.isTraded ? "#dfdfdf" : "#ffffff")};
+  background-color: ${(props) => (props.istraded ? "#dfdfdf" : "#ffffff")};
   box-shadow: 0 1px 2px rgb(0 0 0 / 10%);
   border-radius: 3px;
 `;
